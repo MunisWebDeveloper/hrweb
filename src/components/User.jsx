9dc692e75f541f87df547b0aa78f8a662763a756
Solijonov Alisher <a15793639@gmail.com>
2025-11-20 15:27:35 +0500

    Table bolimi qoshildi

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,6 +2,7 @@ import DateComp from "./helper/DateComp"
 import InputComp from "./helper/InputComp"
 import LabelComp from "./helper/LabelComp"
 import SelectComp from "./helper/SelectComp"
+import ChildrenTable from "./helper/TableComp"
 
 const User = () => {
   return (
@@ -34,6 +35,15 @@ const User = () => {
     </div>
         <LabelComp text={"Oilaviy holatingiz"} />
     <SelectComp/>
+    <div className="flex justify-between">
+        <LabelComp text={"Farzandlari"} />
+      <div className="bg-blue-700 p-1 rounded-[10%] flex items-center justify-center w-10 h-5">
+  <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4"/>
+  </svg>
+</div>
+    </div>
+<ChildrenTable/>
 </div>
     </>
   )
